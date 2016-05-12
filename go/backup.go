a05904b2cb13d0d70bf76605b7552e1ae4ef67ac
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-05-12 15:28:30 +0100

    in progress

--- go/backup.go
+++ go/backup.go
@@ -22,10 +22,44 @@ import (
 	"net"
 	"net/rpc"
 	"os"
+	"strconv"
 )
+
+// Name of the sqlite3 database file
+const DBFILE = "rsyncbackup.db"
+
+// The 'includes' table
+type Include struct {
+	Id     int64
+	TaskId int64
+	Host   string
+	Base   string // Data centre name
+}
+
+// The 'excludes' table
+type Exclude struct {
+	Id        int64
+	IncludeId int64
+	Path      string
+}
+
+// The 'settings' table
+type Setting struct {
+	Id         int64
+	TaskId     int64
+	Protocol   string
+	Pre        string
+	RsyncOpts  string
+	BaseDir    string
+	KnownHosts string
+	NumPeriods int64
+	Timeout    int64
+}
 
 // Create tables and indexes in InitDB
 func (gormInst *GormDB) InitDB(dbname string) error {
+
+	// We just read the database so empty
 
 	return nil
 }
@@ -52,23 +86,143 @@ func (t *Plugin) PostRequest(args *Args, response *[]byte) error {
 
 	// POST requests can change state.
 
-	// Run the backup script.
+	// env_id is required, '?env_id=xxx'
+
+	if len(args.QueryString["env_id"]) == 0 {
+		ReturnError("'env_id' must be set", response)
+		return nil
+	}
+
+	env_id_str := args.QueryString["env_id"][0]
+
+	// task_id is required, '?task_id=xxx'
 
 	if len(args.QueryString["task_id"]) == 0 {
 		ReturnError("'task_id' must be set", response)
 		return nil
 	}
 
-	var cmdargs string
+	task_id_str := args.QueryString["task_id"][0]
 
+	// Check if the user is allowed to access the environment
+	var err error
+	if _, err = t.GetAllowedEnv(args, env_id_str, response); err != nil {
+		// GetAllowedEnv wrote the error
+		return nil
+	}
+
+	// We got this far so access is allowed.
+
+	// Setup/Open the local database
+
+	var gormdb *GormDB
+	if gormdb, err = NewDB(args, DBFILE); err != nil {
+		txt := "NewDB open error for '" + config.DBPath() + DBFILE + "'. " +
+			err.Error()
+		ReturnError(txt, response)
+		return nil
+	}
+
+	db := gormdb.DB() // for convenience
+
+	// Command arguments
+
+	var cmdargs string
 	if len(args.QueryString["verbose"]) > 0 {
 		cmdargs = "-V"
 	}
 
 	// Set up the environment variables
 
-	envvars := `PROTOCOL=rsyncd PRE=create_zfs_snapshot BASEDIR=/backup/servers-zfs/ INCL="phhlapphot001 backup /var/log/** /hcom/scripts/** /hcom/work/** /hcom/docker_volumes/**
+	/* envvars := `PROTOCOL=rsyncd PRE=create_zfs_snapshot BASEDIR=/backup/servers-zfs/ INCL="phhlapphot001 backup /var/log/** /hcom/scripts/** /hcom/work/** /hcom/docker_volumes/**
 phhlapphot002 backup /var/log/** /hcom/scripts/** /hcom/work/** /hcom/docker_volumes/**"`
+        */
+
+	// The string of environment variables
+        var env_vars_str string
+
+	// Get settings
+
+	var setting Setting
+
+	Lock()
+	if err = db.First(&setting, "task_id = ?", task_id_str).Error; err != nil {
+		Unlock()
+		ReturnError("Query error. "+err.Error(), response)
+		return nil
+	}
+	Unlock()
+
+	var space string
+
+	if len(setting.Protocol) > 0 {
+		env_vars_str += space + "PROTOCOL=" + setting.Protocol
+		space = " "
+	}
+	if len(setting.Pre) > 0 {
+		env_vars_str += space + "PRE=" + setting.Pre
+		space = " "
+	}
+	if len(setting.RsyncOpts) > 0 {
+		env_vars_str += space + "RSYNC_OPTS=" + `"` + setting.RsyncOpts + `"`
+		space = " "
+	}
+	if len(setting.BaseDir) > 0 {
+		env_vars_str += space + "BASEDIR=" + `"` + setting.BaseDir + `"`
+		space = " "
+	}
+	if len(setting.KnownHosts) > 0 {
+		env_vars_str += space + "KNOWNHOSTS=" + `"` + setting.KnownHosts + `"`
+		space = " "
+	}
+	env_vars_str += space + "NUMPERIODS=" + strconv.FormatInt(setting.NumPeriods,10)
+	space = " "
+	env_vars_str += space + "TIMEOUT=" + strconv.FormatInt(setting.Timeout,10)
+
+	// Get all includes for this task
+
+	includes := []Include{}
+	Lock()
+	if err = db.Find(&includes, "task_id = ?", task_id_str).Error; err != nil {
+		Unlock()
+		ReturnError("Query error. "+err.Error(), response)
+		return nil
+	}
+	Unlock()
+
+	// Get excludes
+
+        var newline string
+
+	env_vars_str += space + "INCL=\""
+
+	for i := range includes {
+
+		env_vars_str += newline + includes[i].Host + " " + includes[i].Base + " "
+
+		excludes := []Exclude{}
+		Lock()
+		if err = db.Find(&excludes, "include_id = ?", includes[i].Id).Error; err != nil {
+			Unlock()
+			ReturnError("Query error. "+err.Error(), response)
+			return nil
+		}
+		Unlock()
+
+		var space string
+		for j := range excludes {
+			env_vars_str += space + excludes[j].Path
+			space = " "
+		}
+
+		newline = "\n"
+	}
+
+	env_vars_str += "\""
+
+        logit(env_vars_str)
+
+	// Run the backup script.
 
 	sa := ScriptArgs{
 		// The name of the script to send an run
@@ -76,7 +230,7 @@ phhlapphot002 backup /var/log/** /hcom/scripts/** /hcom/work/** /hcom/docker_vol
 		// The arguments to use when running the script
 		CmdArgs: cmdargs,
 		// Environment variables to pass to the script
-		EnvVars: envvars,
+		EnvVars: env_vars_str,
 		// Name of an environment capability (where isworkerdef == true)
 		// that can point to a worker other than the default.
 		EnvCapDesc: "RSYNCBACKUP_WORKER_1",
@@ -133,6 +287,13 @@ func (t *Plugin) HandleRequest(args *Args, response *[]byte) error {
 func main() {
 
 	//logit("Plugin starting")
+
+	// Sets the global config var, needed for PluginDatabasePath
+	NewConfig()
+
+	// Create a lock file to use for synchronisation
+	config.Port = 49995
+	config.Portlock = NewPortLock(config.Port)
 
 	plugin := new(Plugin)
 	rpc.Register(plugin)
